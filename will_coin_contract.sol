e8789ca60824f09e5f9130ba59537e9162aa4454
Richard Jarram <rjarram@me.com>
2019-05-31 13:01:00 -0400

    conract spec setup

--- will_coin_contract.sol
+++ will_coin_contract.sol
@@ -1,17 +1,17 @@
-pragma solidity ^0.4.24;
+// pragma solidity ^0.4.24;
 
 // ----------------------------------------------------------------------------
-// '0Fucks' token contract
+// 'GoodWillToken' token contract
 //
-// Deployed to : 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222
-// Symbol      : 0FUCKS
-// Name        : 0 Fucks Token
+// Deployed to : 0x43BD7C3aB2526956c31e18e42d2C12E22dED6E04
+// Symbol      : GOODWILL
+// Name        : Good Will Token
 // Total supply: 100000000
 // Decimals    : 18
 //
 // Enjoy.
 //
-// (c) by Moritz Neto with BokkyPooBah / Bok Consulting Pty Ltd Au 2017. The MIT Licence.
+// (c) by Richard Jarram / All Good Will is intended for Will Hallmark. Not the Card. The JUUL Licence. Puff-puff game.
 // ----------------------------------------------------------------------------
 
 
@@ -99,7 +99,7 @@ contract Owned {
 // ERC20 Token, with the addition of symbol, name and decimals and assisted
 // token transfers
 // ----------------------------------------------------------------------------
-contract FucksToken is ERC20Interface, Owned, SafeMath {
+contract GoodWillToken is ERC20Interface, Owned, SafeMath {
     string public symbol;
     string public  name;
     uint8 public decimals;
@@ -113,12 +113,12 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
     // Constructor
     // ------------------------------------------------------------------------
     constructor() public {
-        symbol = "0FUCKS";
-        name = "0 Fucks Token";
+        symbol = "GOODWILL";
+        name = "Good Will Token";
         decimals = 18;
         _totalSupply = 100000000000000000000000000;
         balances[0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222] = _totalSupply;
-        emit Transfer(address(0), 0x5A86f0cafD4ef3ba4f0344C138afcC84bd1ED222, _totalSupply);
+        emit Transfer(address(0), 0x43BD7C3aB2526956c31e18e42d2C12E22dED6E04, _totalSupply);
     }
 
 
@@ -182,7 +182,7 @@ contract FucksToken is ERC20Interface, Owned, SafeMath {
         emit Transfer(from, to, tokens);
         return true;
     }
-
+f
 
     // ------------------------------------------------------------------------
     // Returns the amount of tokens approved by the owner that can be
